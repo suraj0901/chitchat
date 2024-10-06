b99281cfb218cc1449c9569947f494188c0b2822
keshav-aham <keshav.takatrao@aham.health>
2024-10-06 15:47:35 +0530

    add dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { ModeToggle } from "@/components/mode-toggle.tsx";
 import PWABadge from "./PWABadge.tsx";
 
 function App() {
   return (
-    <>
+    <div className="container mx-auto">
+      <header className="flex items-center justify-between p-4">
+        <h1 className="font-semibold text-2xl uppercase tracking-wider">
+          ChitChat
+        </h1>
+        <ModeToggle />
+      </header>
       <PWABadge />
-    </>
+    </div>
   );
 }
 
