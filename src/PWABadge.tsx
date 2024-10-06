704c74db135b8a543618c682ee276c2f15b90acc
keshav-aham <keshav.takatrao@aham.health>
2024-10-06 15:39:37 +0530

    Add tailwind css and shadcn Ui

--- src/PWABadge.tsx
+++ src/PWABadge.tsx
@@ -1,78 +1,82 @@
-import './PWABadge.css'
-
-import { useRegisterSW } from 'virtual:pwa-register/react'
+import { useRegisterSW } from "virtual:pwa-register/react";
 
 function PWABadge() {
   // periodic sync is disabled, change the value to enable it, the period is in milliseconds
-// You can remove onRegisteredSW callback and registerPeriodicSync function
-  const period = 0
+  // You can remove onRegisteredSW callback and registerPeriodicSync function
+  const period = 1000;
 
   const {
-    
     needRefresh: [needRefresh, setNeedRefresh],
     updateServiceWorker,
   } = useRegisterSW({
     onRegisteredSW(swUrl, r) {
-      if (period <= 0) return
-      if (r?.active?.state === 'activated') {
-        registerPeriodicSync(period, swUrl, r)
-      }
-      else if (r?.installing) {
-        r.installing.addEventListener('statechange', (e) => {
-          const sw = e.target as ServiceWorker
-          if (sw.state === 'activated')
-            registerPeriodicSync(period, swUrl, r)
-        })
+      console.log("PWABadge");
+      if (period <= 0) return;
+      if (r?.active?.state === "activated") {
+        registerPeriodicSync(period, swUrl, r);
+      } else if (r?.installing) {
+        r.installing.addEventListener("statechange", (e) => {
+          const sw = e.target as ServiceWorker;
+          if (sw.state === "activated") registerPeriodicSync(period, swUrl, r);
+        });
       }
     },
-  })
+  });
 
   function close() {
-    
-    setNeedRefresh(false)
+    setNeedRefresh(false);
   }
 
   return (
     <div className="PWABadge" role="alert" aria-labelledby="toast-message">
-      { (needRefresh)
-      && (
+      {needRefresh && (
         <div className="PWABadge-toast">
           <div className="PWABadge-message">
-            <span id="toast-message">New content available, click on reload button to update.</span>
-              
-              
+            <span id="toast-message">
+              New content available, click on reload button to update.
+            </span>
           </div>
           <div className="PWABadge-buttons">
-            <button className="PWABadge-toast-button" onClick={() => updateServiceWorker(true)}>Reload</button>
-            <button className="PWABadge-toast-button" onClick={() => close()}>Close</button>
+            <button
+              className="PWABadge-toast-button"
+              onClick={() => updateServiceWorker(true)}
+            >
+              Reload
+            </button>
+            <button className="PWABadge-toast-button" onClick={() => close()}>
+              Close
+            </button>
           </div>
         </div>
       )}
     </div>
-  )
+  );
 }
 
-export default PWABadge
+export default PWABadge;
 
 /**
  * This function will register a periodic sync check every hour, you can modify the interval as needed.
  */
-function registerPeriodicSync(period: number, swUrl: string, r: ServiceWorkerRegistration) {
-  if (period <= 0) return
+function registerPeriodicSync(
+  period: number,
+  swUrl: string,
+  r: ServiceWorkerRegistration
+) {
+  if (period <= 0) return;
 
   setInterval(async () => {
-    if ('onLine' in navigator && !navigator.onLine)
-      return
+    if ("onLine" in navigator && !navigator.onLine) return;
+    console.log("Periodic sync check");
 
     const resp = await fetch(swUrl, {
-      cache: 'no-store',
+      cache: "no-store",
       headers: {
-        'cache': 'no-store',
-        'cache-control': 'no-cache',
+        cache: "no-store",
+        "cache-control": "no-cache",
       },
-    })
+    });
 
-    if (resp?.status === 200)
-      await r.update()
-  }, period)
+    if (resp?.status === 200) await r.update();
+  }, period);
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,36 +1,45 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({
-    registerType: 'prompt',
-    injectRegister: false,
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'chitchat',
-      short_name: 'chitchat',
-      description: 'chitchat',
-      theme_color: '#ffffff',
-    },
+      manifest: {
+        name: "chitchat",
+        short_name: "chitchat",
+        description: "chitchat",
+        theme_color: "#ffffff",
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-  })],
-})
+  },
+});
